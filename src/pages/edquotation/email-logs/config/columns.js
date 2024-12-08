0d9f6d5c2384e3d65e1dcd87c1738704d17e147f
Shiva96527 <shiva2691996@gmail.com>
2024-12-08 17:00:44 +0800

    email logs integrated

--- src/pages/edquotation/email-logs/config/columns.js
+++ src/pages/edquotation/email-logs/config/columns.js
@@ -1,7 +1,8 @@
 export const email_log_columns = [
-  { headerName: "Date & Time", field: "", minWidth: 250 },
-  { headerName: "To Email", field: "", minWidth: 250 },
-  { headerName: "CC Email", field: "", minWidth: 250 },
-  { headerName: "Subject", field: "", minWidth: 250 },
-  { headerName: "Body", field: "", minWidth: 250 },
+  { headerName: "Receiver", field: "toEmail", minWidth: 250 },
+  { headerName: "Subject", field: "emailsubject", minWidth: 250 },
+  { headerName: "Body", field: "emailBody", minWidth: 250 },
+  { headerName: "CC Email", field: "ccEmail", minWidth: 250 },
+  { headerName: "Created By", field: "createdBy", minWidth: 250 },
+  { headerName: "Created Date", field: "createdDate", minWidth: 250 },
 ];
--- src/pages/edquotation/email-logs/index.js
+++ src/pages/edquotation/email-logs/index.js
@@ -1,15 +1,37 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import NeptuneAgGrid from "../../../components/ag-grid";
 import { Col, Row } from "reactstrap";
 import { email_log_columns } from "./config/columns";
+import { useSelector } from "react-redux";
+import { getDigitalQuoteDetail } from "../helper";
 
 export default function EmailLogs() {
   const [emailList, setEmailList] = useState([]);
+  const { digitalizeQuoteId } = useSelector((state) => state?.globalSlice);
+
+  useEffect(() => {
+    getQuoteDetail(digitalizeQuoteId);
+  }, []);
+
+  useEffect(() => {
+    getQuoteDetail(digitalizeQuoteId);
+  }, [digitalizeQuoteId]);
+
+  const getQuoteDetail = async () => {
+    const quoteDetail = await getDigitalQuoteDetail(digitalizeQuoteId);
+    console.log(
+      "quoteDetail,quoteDetail.mailLog",
+      quoteDetail,
+      quoteDetail.mailLog
+    );
+    setEmailList(quoteDetail?.mailLog);
+  };
+
   return (
     <Row className="m-3">
       <Col md={12}>
         <NeptuneAgGrid
-          refId={"srf-workflow"}
+          refId={"ed-email-workflow"}
           data={emailList}
           dataprops={email_log_columns}
           paginated={true}
