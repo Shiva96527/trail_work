a8866088777232e8760198aeaf73dc0b101d30b7
Shiva96527 <shiva2691996@gmail.com>
2024-12-21 20:12:26 +0530

    email logs html, drop feature added

--- src/pages/edquotation/task-history/index.js
+++ src/pages/edquotation/task-history/index.js
@@ -3,9 +3,12 @@ import workflow_columns from "./config/column";
 import NeptuneAgGrid from "../../../components/ag-grid";
 import { getDigitalQuoteDetail } from "../helper";
 import { useSelector } from "react-redux";
+import { Button } from "reactstrap";
+import { postDigitalizeQuoteOverallCostingApprovalorReject } from "../../../services/ed-service.js";
 
 export default function EdTaskHistory() {
   const [workflowList, setWorkflowList] = useState([]);
+  const [enableDropButtonFlag, setEnableDropButtonFlag] = useState([]);
   const { digitalizeQuoteId } = useSelector((state) => state?.globalSlice);
 
   useEffect(() => {
@@ -13,14 +16,43 @@ export default function EdTaskHistory() {
   }, [digitalizeQuoteId]);
 
   const getQuoteDetail = async () => {
-    const quoteDetail = await getDigitalQuoteDetail(digitalizeQuoteId);
-    setWorkflowList(quoteDetail?.workFlowResponse);
+    try {
+      const quoteDetail = await getDigitalQuoteDetail(digitalizeQuoteId);
+      setEnableDropButtonFlag(
+        quoteDetail?.quoteCreationResponse?.enableDropButtonFlag
+      );
+      setWorkflowList(quoteDetail?.workFlowResponse);
+    } catch (e) {
+      toast.error("Something went wrong");
+      navigate("/neptune/edquotation/inbox");
+    }
+  };
+
+  const handleDrop = async () => {
+    const payload = {
+      LoginUIID: sessionStorage.getItem("uiid"),
+      type: "drop",
+      digitalizeQuoteId,
+    };
+    try {
+      const {
+        data: { statusCode, statusMessage },
+      } = await postDigitalizeQuoteOverallCostingApprovalorReject(payload);
+      if (statusCode === 200) {
+        toast.success(statusMessage);
+        navigate("/neptune/edquotation/inbox");
+      } else {
+        toast.error(statusMessage);
+      }
+    } catch (e) {
+      toast.error("Something went wrong");
+    }
   };
 
   return (
     <div
       style={{
-        marginTop: "16px",
+        marginTop: "24px",
         marginLeft: "27px",
         marginRight: "27px",
       }}
@@ -33,6 +65,21 @@ export default function EdTaskHistory() {
         itemsPerPage={10}
         searchable={true}
         exportable={true}
+        topActionButtons={
+          <>
+            {enableDropButtonFlag !== "Yes" ? (
+              <Button
+                color="primary"
+                size="sm"
+                data-toggle="tooltip"
+                title="Drop"
+                onClick={handleDrop}
+              >
+                Drop
+              </Button>
+            ) : null}
+          </>
+        }
       />
     </div>
   );
